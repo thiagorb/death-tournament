e21f05a13b448607183218222f1596a93338a1c5
Thiago Barcala <thiago.barcala@gmail.com>
2022-07-29 21:56:10 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: './src/main.ts',
+    output: {
+        filename: 'main.js'
+    },
+    devServer: {
+        hot: true,
+        liveReload: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './index.html',
+            favicon: './assets/favicon.ico',
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+                minifyCSS: true,
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".png"]
+    },
+};
